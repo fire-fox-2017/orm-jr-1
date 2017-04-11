d04f4ec5e45e7ec3bb637c53c68ca9ebf7919fb5
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-11 10:48:51 +0700

    initial commit

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+
--- models/db_model.js
+++ models/db_model.js
@@ -1,7 +1,22 @@
 "use strict"
 
-class DBModel {
+const repl = require('repl');
+const sqlite = require('sqlite3').verbose();
 
+class DBModel {
+  constructor(file = '.db/students.db') {
+    this.file = file;
+    this.connection = new sqlite.Database(this.file);
+  }
+
+  static createTable() {
+    const db = this.connection;
+    const CREATE_STUDENT = "CREATE TABLE IF NOT EXISTS students ( id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, birthdate DATE );";
+    const CREATE_COHORT = "CREATE TABLE IF NOT EXISTS cohorts ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL );";
+    
+    
+
+  }
 
 }
 
