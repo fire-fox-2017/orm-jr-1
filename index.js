cb5734a211f5ddc3487e068c0d156d184a43e632
name": null <email": null>
2017-04-11 12:19:46 +0700

    created table student

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+
+
+let dbModel = new DBModel();
+
+const repl = require('repl');
+const replServer = repl.start({prompt: '> '});
+replServer.context.dbModel = dbModel;
--- models/db_model.js
+++ models/db_model.js
@@ -1,6 +1,33 @@
 "use strict"
 
+
+
 class DBModel {
+  constructor () {
+    const sqlite = require('sqlite3').verbose();
+    // this.filename = './db/student.db';
+    this.connection = new sqlite.Database('./db/student.db');
+  }
+
+  setup() {
+    // create table student
+    let create_table_student_str = `CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL )`;
+
+    let db = this.connection;
+    db.serialize(function () {
+      db.run(create_table_student_str, function (err) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log('Created TABLE Students Successfully.');
+        }
+      });
+    });
+
+
+    // create table cohort
+  }
 
 
 }
