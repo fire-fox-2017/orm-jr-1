932d5dfe902bf4ce1f69d65645e3e7351c47050b
LeCodeBae <billivank_96@yahoo.co.id>
2017-04-11 21:48:11 +0700

    beres

--- models/student.js
+++ models/student.js
@@ -1,8 +1,60 @@
 "use strict"
 
 class Student {
-
-
+  constructor(firstname, lastname, cohort_id, id) {
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.cohort_id=cohort_id;
+    this.id=id;
+  }
+  static create(db, obj) {
+    db.serialize(function() {
+     db.run(`INSERT INTO students (firstname, lastname, cohort_id) VALUES ('${obj.firstname}', '${obj.lastname}', '${obj.cohort_id}');`, function(err) {
+       if (err) {
+         console.log(err.message);
+       } else {
+         console.log('STUDENT ADDED');
+       }
+     });
+   });
+  }
+  static update(db, obj) {
+    db.serialize(function() {
+      db.run(`UPDATE students SET firstname = '${obj.firstname}', lastname = '${obj.lastname}', cohort_id = '${obj.cohort_id}' WHERE id = ${obj.id};`, function(err) {
+        if (err) {
+          console.log(err.message);
+        } else {
+          console.log('STUDENT UPDATED');
+        }
+      });
+    });
+  }
+  static delete(db, id) {
+    db.serialize(function() {
+      db.run(`DELETE FROM students WHERE id = ${id};`, function(err) {
+        if (err) {
+          console.log(err.message);
+        } else {
+          console.log('STUDENT DELETED');
+        }
+      });
+    });
+  }
+  static findById(db, id) {
+    db.each(`SELECT * FROM students WHERE id = '${id}';`, function(err,data) {
+       if (err) {
+         console.log(err.message);
+       } else {
+         console.log(data);
+       }
+    });
+  }
+  static findAll(db, callback) {
+    db.all('SELECT * FROM students', callback)
+  }
+  static where(db, condition, callback) {
+    db.all(`SELECT * FROM students WHERE ${condition}`, callback)
+  }
 }
 
 export default Student
--- test/simple.js
+++ test/simple.js
@@ -1,4 +1,23 @@
 import DBModel from "../models/db_model.js";
 import Student from "../models/student.js";
-
+const sqlite = require('sqlite3').verbose();
 var db = new DBModel("./db/test.db")
+db.setup()
+Student.create(db.connection, new Student("Bill", "Koo", 1))
+console.log("Inserting new student...")
+db.connection.each(`SELECT * FROM students WHERE id = 1;`, function(err,data) {
+   if (!err && typeof data === 'object') {
+     console.log('test create student : success');
+   } else {
+     console.log('test create student : fail');
+   }
+});
+Student.update(db.connection, new Student("Zani", "Akbar", 2, 1))
+console.log("UPDATE DATA")
+db.connection.each('select * from students where firstname = "Zani" and id = 1', function (err, data){
+  if(!err && typeof data === 'object'){
+    console.log('test update student : success')
+  } else {
+    console.log('test update student : fail')
+  }
+})
