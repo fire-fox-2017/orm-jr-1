2fc52b8aa30915d33b105edc78a57b898f4297af
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-13 07:51:16 +0700

    done until test first push

--- test/simple.js
+++ test/simple.js
@@ -5,21 +5,22 @@ import Student from "../models/student.js";
 import Cohort from "../models/cohort.js";
 
 const sqlite = require('sqlite3').verbose();
-var db = new DBModel('./db/test.db');
-// db.setup();
-console.log(db.connection);
 
-// Student.create(db.connection, new Student('Roberto','Firmino', 2));
+var dbModel = new DBModel('../db/test.db');
+dbModel.setup();
+console.log(dbModel.connection);
+
+// Student.create(dbModel.connection, new Student('Roberto','Firmino', 2));
 // let testCreate = `SELECT * FROM Students WHERE firstname = 'Roberto' AND lastname = 'Firmino' AND cohort_id = 2`
-// db.connection.each(testCreate,(err,row)=>{
+// dbModel.connection.each(testCreate,(err,row)=>{
 //   if(typeof row == 'object'){
 //     console.log(`Test create student : success`);
 //   }
 // });
 
-// Student.update(db.connection, new Student('Roberto', 'Asalamakete', 2));
+// Student.update(dbModel.connection, new Student('Roberto', 'Asalamakete', 2));
 // let testUpdate = `SELECT * FROM Students WHERE firstname = 'Roberto' AND lastname = 'Firmino' AND cohort_id = 2`
-// db.connection.each(testUpdate,(err,row)=>{
+// dbModel.connection.each(testUpdate,(err,row)=>{
 //   if(typeof row == 'object'){
 //     console.log(`Test create student : success`);
 //   }
