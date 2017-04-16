fba02818b532e91199df0f968a10e835ba9023e0
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-04-16 13:23:29 +0700

    add help

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ let replServer = repl.start({
     prompt: '> '
 })
 
+function help(){
+    console.log('help() : menampilkan daftar help');
+    console.log('Menu Siswa\n')
+    console.log('Student.create() : menambah data siswa');
+    console.log('Student.update() : mengupdate data siswa');
+    console.log('Student.delete() : menghapus data siswa');
+    console.log('Student.findById() : mencari data siswa berdasarkan id');
+    console.log('Student.findAll() : mencari data seluruh siswa');
+    console.log('Student.where() : menambah data siswa berdasarkan atribut tertentu\n');
+    console.log('Menu Cohort\n')
+    console.log('Cohort.create() : menambah data cohort');
+    console.log('Cohort.update() : mengupdate data cohort');
+    console.log('Cohort.delete() : menghapus data cohort');
+    console.log('Cohort.findById() : mencari data cohort berdasarkan id');
+    console.log('Cohort.findAll() : mencari data seluruh cohort');
+    console.log('Cohort.where() : menambah data cohort berdasarkan atribut tertentu\n');
+}
+
 replServer.context.dbModel = new DBModel("./db/student.db")
 replServer.context.Student = Student
 replServer.context.Cohort = Cohort
+replServer.context.help = help
 
-// Student.where(dbModel.connection, "first_name = 'Joko'",  function(data, err) {if(!err) {console.log(data);} else { console.log(err); }})
