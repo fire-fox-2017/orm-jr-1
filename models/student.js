3d9031cc956a55871425fa85f7ef1b521c715d80
jamessraun <jeezybig@gmail.com>
2017-04-17 15:35:58 +0700

    first commit

--- models/student.js
+++ models/student.js
@@ -1,7 +1,92 @@
 "use strict"
 
 class Student {
+  constructor(firstname,lastname,id){
+    this.fname=firstname;
+    this.lname=lastname;
+    this.id = id;
+  }
 
+  static create(connection,objStudent){
+    let db = connection;
+    var query = `INSERT INTO student (firstname,lastname,cohort_id) VALUES ('${objStudent.fname}','${objStudent.lname}','${objStudent.id}');`;
+    db.serialize(function(){
+      db.run(query, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log(`${objStudent.fname}`);
+        }
+      });
+    });
+  }
+
+  static update(connection,objStudent)
+  {
+    let db= connection;
+    var query = `UPDATE student set firstname='${objStudent.fname}',lastname='${objStudent.lname}',cohort_id='${objStudent.id}' WHERE id='${objStudent.id}';`;
+    db.serialize(function(){
+      db.run(query, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log(`${objStudent.fname}`);
+        }
+      });
+    });
+  }
+
+  static delete(connection,kondisi){
+    let db= connection;
+    var query = `DELETE FROM student WHERE id = '${kondisi}';`;
+    db.serialize(function(){
+      db.run(query, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log(` Data has been Deleted`);
+        }
+      });
+    });
+  }
+
+  static findById (connection, id) {
+    let db = connection;
+    let query = `SELECT * from student where id = ${id}`;
+    db.each(query, function(err, row) {
+      if(err)
+      console.log(err);
+      else {
+        console.log(row);
+        let student = new Student(row.id, row.firstname, row.lastname, row.cohort_id);
+        return student;
+      }
+
+    });
+
+  }
+
+  static findAll(connection, callback) {
+    let db = connection;
+    let query = `SELECT * from student`;
+    db.all(query, function(err, rows) {
+      if(err){
+        callback(err,null)
+      }else callback(rows,null);
+    });
+  }
+
+  static where(connection, kondisi, callback) {
+    let db = connection;
+    let query = `SELECT * from student where ${kondisi}`;
+    db.all(query, function(err, rows) {
+      if(err)
+      {
+        callback(err,null);
+      }else callback(rows,null);
+    });
+
+  }
 
 }
 
